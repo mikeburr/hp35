aa55d62f1bc239585fd02298eeaebe36fd47ec27
Mike Burr <mike.j.burr@gmail.com>
2026-03-04 21:40:39 -0500

    add multi-button LED display

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,4 +1,11 @@
-import { default as streamDeck, action, KeyDownEvent, SingletonAction, WillAppearEvent } from "@elgato/streamdeck";
+import {
+    default as streamDeck,
+    action,
+    type KeyAction,
+    KeyDownEvent,
+    SingletonAction,
+    WillAppearEvent
+} from "@elgato/streamdeck";
 import { type Display as HP35Display, type HP35 } from '../hp35'
 import { JsonObject } from "@elgato/utils";
 
@@ -31,6 +38,51 @@ function hp35Digit(value: number) {
 }
 
 /**
+ *   T T T T T UR
+ *   UL        UR
+ *   UL        UR
+ *   UL        UR
+ *   UL        UR
+ *   ULM M M M UR
+ *   LL        LR
+ *   LL        LR
+ *   LL        LR
+ *   LL        LR
+ *   B B B B B LR
+ */
+
+const Segments = {
+    TOP: `M 0 0 L 5 0 L 5 1 L 0 1 Z`,
+    UL: `M 0 1 L 1 1 L 1 6 L 0 6 Z`,
+    UR: `M 5 0 L 6 0 L 6 6 L 5 6 Z`,
+    MID: `M 1 5 L 5 5 L 5 6 L 1 6 Z`,
+    LL: `M 0 6 L 1 6 L 1 10 L 0 10 Z`,
+    BOT: `M 0 10 L 5 10 L 5 11 L 0 11 Z`,
+    LR: `M 5 6 L 6 6 L 6 11 L 5 11 Z`,
+    DOT: `M 1.5 8 L 3.5 8 L 3.5 10 L 1.5 10 Z`,
+}
+
+const character = (...segments: Array<keyof typeof Segments>) =>
+    (svgAttrs: string) =>
+        `<svg ${svgAttrs} viewBox="0 0 6 11"><path d="${segments.map(seg => Segments[seg]).join(' ')}"/></svg>`
+
+const Characters = {
+    '0': character('TOP','UL','UR','LL','LR','BOT'),
+    '1': character('UR','LR'),
+    '2': character('TOP','UR','MID','LL','BOT'),
+    '3': character('TOP','UR','MID','LR','BOT'),
+    '4': character('UL','UR','MID','LR'),
+    '5': character('TOP','UL','MID','LR','BOT'),
+    '6': character('TOP','UL','MID','LL','LR','BOT'),
+    '7': character('TOP','UR','LR'),
+    '8': character('TOP','UL','UR','MID','LL','LR','BOT'),
+    '9': character('TOP','UL','UR','MID','LR','BOT'),
+    '-': character('MID'),
+    '.': character('DOT'),
+    ' ': character(),
+}
+
+/**
  * 
  */
 @action({ UUID: "org.mikeburr.hp35.display" })
@@ -40,21 +92,97 @@ export class Display extends SingletonAction {
         this.hp35.addDisplayListener(x => this.update(x))
     }
 
-    stringify(d: HP35Display) {
-        const result =
-            (d.sign !== ' ' ? d.sign : '') +
-            d.mantissa +
-            (d.exponent !== '' ? d.exponentSign + d.exponent : '')
-        streamDeck.logger.debug(`display: ${JSON.stringify(d)} => '${result}'`)
-        return result
+    svgify(...strings: string[]) {
+        const width = Math.max(...strings.map(s => s.length))
+
+        let svg =
+            `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
+                <svg viewBox="0 0 ${0.25 + width * 1.25} ${strings.length * 1.5 - 0.5}" fill="#f13e33">
+                    ${strings.map((s, si) =>
+                        [...s].map((c, ci) =>
+                            Characters[c as keyof typeof Characters](`x="${0.25 + ci * 1.25}" y="${si * 1.5}" height="1" width="1"`)
+                        ).join('')
+                    ).join('\n')}
+                </svg>
+            </svg>`
+
+        streamDeck.logger.info(`svgify: ${JSON.stringify(strings)} => ${svg}`)
+		return `data:image/svg+xml;charset=utf8,${encodeURIComponent(svg)}`
     }
 
+    // TODO - handle error states
+
     override onWillAppear(ev: WillAppearEvent<JsonObject>): Promise<void> | void {
-        ev.action.setTitle(this.stringify(this.hp35.x))
+        ev.action.setTitle('')
+        setTimeout(() => this.update({ error: false, sign: ' ', mantissa: '0.', exponentSign: ' ', exponent: '' }), 100)
     }
 
     update(display: HP35Display) {
-        const me = this.actions.forEach(action => action.setTitle(this.stringify(display)))
+        const s = display.sign + display.mantissa.padEnd(11) + display.exponentSign + display.exponent.padEnd(2)
+
+        const actions = Array.from(this.actions).filter(a => a.coordinates).sort((a1, a2) =>
+            Math.sign((a1.coordinates!.row * 100 + a1.coordinates!.column) -
+                      (a2.coordinates!.row * 100 + a2.coordinates!.column))
+        ) as Array<KeyAction>
+
+        for (let i = 0; i < actions.length; ) {
+            const group = [actions[i]]
+            let { row, column } = actions[i].coordinates!
+            i++
+
+            while (group.length < 3 &&
+                   i < actions.length &&
+                   actions[i].coordinates!.row === row &&
+                   actions[i].coordinates!.column === column + 1) {
+                group.push(actions[i])
+                column++
+                i++
+            }
+
+            streamDeck.logger.info(`Display.update: found group of buttons at ${JSON.stringify(group.map(g => `${g.coordinates!.row}/${g.coordinates!.column}`))}`)
+
+            // possible geometries
+            // single display button:
+            //    - 1 2 3 4
+            //    5 6 7 8 9
+            //    0 . - 1 2
+            //
+            // 2 horizontally adjacent buttons:
+            //    - 1 2 3   4 5 6 7
+            //    8 9 0 .   - 1 2
+            //
+            // 3 horizontally adjacent buttons:
+            //    - 1 2 3 4   5 6 7 8 9   0 . - 1 2
+
+            if (group.length === 1) {
+                group[0].setImage(this.svgify(
+                    s.substring(0, 5),
+                    s.substring(5, 10),
+                    s.substring(10, 15),
+                ))
+            }
+            else if (group.length === 2) {
+                group[0].setImage(this.svgify(
+                    s.substring(0, 4),
+                    s.substring(8, 12),
+                ))
+                group[1].setImage(this.svgify(
+                    s.substring(4, 8),
+                    s.substring(12, 15),
+                ))
+            }
+            else { // group.length === 3
+                group[0].setImage(this.svgify(
+                    s.substring(0, 5),
+                ))
+                group[1].setImage(this.svgify(
+                    s.substring(5, 10),
+                ))
+                group[2].setImage(this.svgify(
+                    s.substring(10, 15),
+                ))
+            }
+        }
     }
 }
 
